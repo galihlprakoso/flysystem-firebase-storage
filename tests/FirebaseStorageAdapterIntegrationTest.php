9c93b4f15a195a8388c15d8b7272b7f25e8a46a7
Galih Laras Prakoso <galih.prakoso@yourpay.co.id>
2024-09-20 10:50:29 +0700

    test: added integration testing on firebase storage adapter

---
+++ tests/FirebaseStorageAdapterIntegrationTest.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace galihlprakoso\Adapters;
+
+use PHPUnit\Framework\TestCase;
+use Kreait\Firebase\Factory;
+use League\Flysystem\Config;
+
+class FirebaseStorageAdapterTest extends TestCase
+{
+    protected FirebaseStorageAdapter $adapter;
+
+    protected function setUp(): void
+    {
+        $factory = (new Factory())->withServiceAccount(__DIR__ . '/<service-account-name>.json');
+        $storageClient = $factory->createStorage();
+
+        $this->adapter = new FirebaseStorageAdapter($storageClient, 'janjipolitisi.appspot.com');
+    }
+
+    public function testUploadFileToFirebaseStorage(): void
+    {
+        $path = 'test/test-file.txt';
+        $contents = 'Hello, Firebase!';
+
+        $this->adapter->write($path, $contents, new Config());
+
+        $this->assertTrue($this->adapter->fileExists($path));
+
+        $this->adapter->delete($path);
+    }
+
+    public function testReadFileFromFirebaseStorage(): void
+    {
+        $path = 'test/test-file.txt';
+        $contents = 'Hello, Firebase!';
+
+        $this->adapter->write($path, $contents, new Config());
+
+        $retrievedContents = $this->adapter->read($path);
+
+        $this->assertEquals($contents, $retrievedContents);
+
+        $this->adapter->delete($path);
+    }
+}
